755e7dc12cec08ab5d8878557c30e8750e7bfca8
fdai4076 <sebastian.krah2@informatik.hs-fulda.de>
2018-06-22 11:49:49 +0200

    prototype player
    prototype player
    instantiate players
    dead players
    set bomb

---
+++ Prototype_Player/Prototype_Player/Assets/Scripte/PlayerSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour {
+
+    public GameObject playerPrefab;
+    public int players;
+    public List<GameObject> playerList;
+    Vector3 spawnPosition;
+    List<Color> playerColorList;
+
+	// Use this for initialization
+	void Start ()
+    {
+        spawnPosition = new Vector3(-2, 0, 0);
+        playerList = new List<GameObject>();
+
+        playerColorList = new List<Color>();
+        playerColorList.Add(new Color(1, 0, 0));
+        playerColorList.Add(new Color(0, 1, 0));
+        playerColorList.Add(new Color(0, 0, 1));
+        playerColorList.Add(new Color(0, 0, 0));
+
+        for (int i = 0; i < players; i++)
+        {
+            GameObject tmpPlayer = Instantiate(playerPrefab, spawnPosition, Quaternion.identity);
+            tmpPlayer.name = "Player_" + i.ToString();
+            tmpPlayer.GetComponent<Renderer>().material.color = playerColorList[i];
+            playerList.Add(tmpPlayer);
+            playerList[i].GetComponent<PlayerScript>().setPlayerID(i);
+            playerList[i].GetComponent<PlayerScript>().setPlayerList(playerList);
+            spawnPosition.x += 2;
+        }
+    }
+}
