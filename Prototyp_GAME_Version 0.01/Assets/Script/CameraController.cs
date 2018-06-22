7c30ef522305684881164930700865e80208ec21
Joshua Hirsch <40173207+ItsJoshuaHirsch@users.noreply.github.com>
2018-06-22 17:14:27 +0200

    Camera Zoom mit Y-Achse statt FOV

--- Prototyp_GAME_Version 0.01/Assets/Script/CameraController.cs
+++ Prototyp_GAME_Version 0.01/Assets/Script/CameraController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
     public WorldScript World;
     public List<Transform> players;
@@ -20,13 +21,13 @@ public class CameraController : MonoBehaviour {
     private float levelWidth;
     private float levelHeight;
 
-    private float minFOV = 30f;
-    private float maxFOV = 10f;
+    private float maxHeight;
+    private float minHeight = 20f;
 
 
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         cam = GetComponent<Camera>();
         players.Add(GameObject.Find("Player 1").transform);
@@ -36,38 +37,37 @@ public class CameraController : MonoBehaviour {
         //....
         levelWidth = World.levelBreite;
         levelHeight = World.levelTiefe;
-        this.transform.position = new Vector3(levelWidth / 2, levelWidth * (levelHeight * 0.07f), levelHeight / 2);
-        offset = new Vector3(0, levelWidth * (levelHeight * 0.07f) , 0);
+        maxHeight = levelWidth * (levelHeight * 0.07f);
+        this.transform.position = new Vector3(levelWidth / 2, maxHeight, levelHeight / 2);
+
+        offset = new Vector3(0, maxHeight, 0);
     }
 
     private void LateUpdate()
     {
-        if(players.Count == 0)
+        if (players.Count == 0)
         {
             return;
         }
-        
-        CameraMoving();
-        CameraZooming();
-        
+        if (Time.fixedTime > 3)
+        {
+            CameraMoving();
+        }
+
+
     }
 
     void CameraMoving()
     {
         Vector3 centerPoint = CalcCenterPoint();
 
+        float zoom = Mathf.Lerp(minHeight, maxHeight, GetGreatestDistance() / (levelWidth + 1));
+        float dist = Mathf.Lerp(transform.position.y, zoom, speed * Time.deltaTime);
+        offset = new Vector3(0, dist, 0);
         Vector3 endPos = centerPoint + offset;
         Vector3 smoothPos = Vector3.Lerp(transform.position, endPos, speed * Time.deltaTime);
-        //Vector3 smoothPos2 = Vector3.SmoothDamp(transform.position, endPos, ref vel, speed/10 * Time.deltaTime);
-        
-        transform.position = smoothPos;
-        //transform.LookAt(new Vector3(transform.position.x, player.transform.position.y, transform.position.z));
-    }
 
-    void CameraZooming()
-    {
-        float zoom = Mathf.Lerp(maxFOV, minFOV, GetGreatestDistance() / (levelWidth + 1));
-        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, zoom, Time.deltaTime / 3);
+        transform.position = smoothPos;
     }
 
     float GetGreatestDistance()
@@ -82,7 +82,7 @@ public class CameraController : MonoBehaviour {
             for (int j = i + 1; j < players.Count; j++)
             {
                 float dist = Vector3.Distance(players[i].position, players[j].position);
-                if(dist > maxDist)
+                if (dist > maxDist)
                 {
                     maxDist = dist;
                 }
