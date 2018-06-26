cc7f535a18931e71143cab181c38696ee1054fc1
foxytocin <info@anfuchs.de>
2018-06-26 08:02:15 +0200

    Update

---
+++ Prototyp_GAME_Version 0.03/Assets/Script/CameraScroller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScroller : MonoBehaviour {
+
+    public WorldScript WorldScript;
+    public int rowPosition;
+    public int altePosition;
+    private bool create; 
+
+	// Use this for initialization
+	void Start () {
+        altePosition = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, 0, 0.5f * Time.deltaTime);
+
+	}
+
+    private void LateUpdate()
+    {
+        rowPosition = (int)Mathf.Round(transform.position.z) + 32;
+
+        if (rowPosition > altePosition)
+        {
+            altePosition = rowPosition;
+            WorldScript.createWorld(rowPosition);
+            Debug.Log("GERADE REIHE NR " + rowPosition);
+        }
+    }
+}
---
+++ Prototyp_GAME_Version 0.03/Assets/Script/ColliderScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ColliderScript : MonoBehaviour {
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Wand")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
