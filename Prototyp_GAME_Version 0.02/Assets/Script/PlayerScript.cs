41e774396408d7bf3dcc8d6caf42d971180a8c0a
fdai4076 <sebastian.krah2@informatik.hs-fulda.de>
2018-06-28 14:26:37 +0200

    Collision

--- Prototyp_GAME_Version 0.02/Assets/Script/PlayerScript.cs
+++ Prototyp_GAME_Version 0.02/Assets/Script/PlayerScript.cs
@@ -17,9 +17,11 @@ public class PlayerScript : MonoBehaviour
     public bool aLife;
     public bool remoteBomb;
     public List<GameObject> playerList;
+    public List<bool> himmelsRichtungen;
 
     float speedMultiply;
 
+    
     // Use this for initialization
     void Awake()
     {
@@ -34,82 +36,155 @@ public class PlayerScript : MonoBehaviour
         aLife = true;
         remoteBomb = false;
         speedMultiply = 0.01f;
-
+        himmelsRichtungen.Add(true);//Norden
+        himmelsRichtungen.Add(true);//Osten
+        himmelsRichtungen.Add(true);//Süden
+        himmelsRichtungen.Add(true);//Westen
     }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        Vector3 collisionVector = new Vector3(Mathf.Round(collision.transform.position.x - playerList[playerID].transform.position.x), collision.transform.position.y - playerList[playerID].transform.position.y, Mathf.Round(collision.transform.position.z - playerList[playerID].transform.position.z));
+        Debug.Log(collisionVector.ToString());
+
+        if (collisionVector.x == 1f)
+        {
+            himmelsRichtungen[1] = false;
+            body.transform.Translate(-0.05f, 0, 0);
+        }
+
+        if (collisionVector.x == -1f)
+        {
+            himmelsRichtungen[3] = false;
+            body.transform.Translate(0.05f, 0, 0);
+        }
+
+        if (collisionVector.z == 1f)
+        {
+            himmelsRichtungen[0] = false;
+            body.transform.Translate(0, 0, -0.05f);
+        }
+
+        if (collisionVector.z == -1f)
+        {
+            himmelsRichtungen[2] = false;
+            body.transform.Translate(0, 0, 0.05f);
+        }
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        Vector3 collisionVector = new Vector3(Mathf.Round(collision.transform.position.x - playerList[playerID].transform.position.x), collision.transform.position.y - playerList[playerID].transform.position.y, Mathf.Round(collision.transform.position.z - playerList[playerID].transform.position.z));
+        Debug.Log(collisionVector.ToString());
+
+        if (collisionVector.x < 0f)
+            himmelsRichtungen[3] = true;
+        else
+            himmelsRichtungen[1] = true;
+
+
+        if (collisionVector.z < 0f)
+            himmelsRichtungen[2] = true;
+        else
+            himmelsRichtungen[0] = true;
+    }
+
+
 
     void Update()
     {
         // Player Steuerung
-        if (playerID == 0)
-        {
-            Vector3 tmp = InputManager.OneMainJoystick();
-            if (tmp != new Vector3(0, 0, 0))
+        
+            Vector3 tmp;
+            switch (playerID)
             {
-                speedMulti();
-                wallTest(playerList[0]);
-                //Debug.Log("move" + body.name);
-                body.transform.Translate(tmp.x * speedMultiply  * Time.deltaTime, 0 , tmp.z * speedMultiply * Time.deltaTime);
-            }
-            else if (speedMultiply > 0.1f)
-            {
-                speedMultiply -= 0.1f;
-                //Debug.Log("Increase SM: " +speedMultiply);
+                case 0:
+                
+                    tmp = InputManager.OneMainJoystick();
+
+                    if ((!himmelsRichtungen[0] && tmp.z > 0) || (!himmelsRichtungen[2] && tmp.z < 0))
+                    {
+                        tmp.z = 0f;
+                    }
+
+                    if ((!himmelsRichtungen[1] && tmp.x > 0) || (!himmelsRichtungen[3] && tmp.x < 0))
+                    {
+                        tmp.x = 0f;
+                    }
+                    
+                    if (tmp != new Vector3(0, 0, 0))
+                    {
+                    Debug.Log("Move" + tmp.ToString());
+                        speedMulti();
+                        wallTest(playerList[0]);
+                        //Debug.Log("move" + body.name);
+                        body.transform.Translate(tmp.x * speedMultiply * Time.deltaTime, 0, tmp.z * speedMultiply * Time.deltaTime);
+                    }
+
+                    else if (speedMultiply > 0.1f)
+                    {
+                        speedMultiply -= 0.1f;
+                        //Debug.Log("Increase SM: " +speedMultiply);
+                    }
+                    break;
+
+                case 1:
+
+                    tmp = InputManager.TwoMainJoystick();
+                    if (tmp != new Vector3(0, 0, 0))
+                    {
+                        speedMulti();
+                        wallTest(playerList[1]);
+                        //Debug.Log("move" + body.name);
+                        body.transform.Translate(tmp.x * speedMultiply * Time.deltaTime, 0, tmp.z * speedMultiply * Time.deltaTime);
+                    }
+                    else if (speedMultiply > 0.1f)
+                    {
+                        speedMultiply -= 0.1f;
+                        //Debug.Log("Increase SM: " +speedMultiply);
+                    }
+                    break;
+
+                case 2:
+
+                    tmp = InputManager.ThreeMainJoystick();
+                    if (tmp != new Vector3(0, 0, 0))
+                    {
+                        speedMulti();
+                        wallTest(playerList[2]);
+                        //Debug.Log("move" + body.name);
+                        body.transform.Translate(tmp.x * speedMultiply * Time.deltaTime, 0, tmp.z * speedMultiply * Time.deltaTime);
+                    }
+                    else if (speedMultiply > 0.1f)
+                    {
+                        speedMultiply -= 0.1f;
+                        //Debug.Log("Increase SM: " +speedMultiply);
+                    }
+                    break;
+
+                case 3:
+                    tmp = InputManager.FourMainJoystick();
+                    if (tmp != new Vector3(0, 0, 0))
+                    {
+                        speedMulti();
+                        wallTest(playerList[3]);
+                        //Debug.Log("move" + body.name);
+                        body.transform.Translate(tmp.x * speedMultiply * Time.deltaTime, 0, tmp.z * speedMultiply * Time.deltaTime);
+                    }
+                    else if (speedMultiply > 0.1f)
+                    {
+                        speedMultiply -= 0.1f;
+                        //Debug.Log("Increase SM: " +speedMultiply);
+                    }
+                    break;
+
+                default:
+                    Debug.Log("Steurungsfehler!");
+                    break;
             }
 
-        }
 
-        if (playerID == 1)
-        {
-            Vector3 tmp = InputManager.TwoMainJoystick();
-            if (tmp != new Vector3(0, 0, 0))
-            {
-                speedMulti();
-                wallTest(playerList[1]);
-                //Debug.Log("move" + body.name);
-                body.transform.Translate(tmp.x * speedMultiply * Time.deltaTime, 0, tmp.z * speedMultiply * Time.deltaTime);
-            }
-            else if (speedMultiply > 0.1f)
-            {
-                speedMultiply -= 0.1f;
-                //Debug.Log("Increase SM: " +speedMultiply);
-            }
-        }
-
-        if (playerID == 2 && (moveX || moveZ))
-        {
-            Vector3 tmp = InputManager.ThreeMainJoystick();
-            if (tmp != new Vector3(0, 0, 0))
-            {
-                speedMulti();
-                wallTest(playerList[2]);
-                //Debug.Log("move" + body.name);
-                body.transform.Translate(tmp.x * speedMultiply * Time.deltaTime, 0, tmp.z * speedMultiply * Time.deltaTime);
-            }
-            else if (speedMultiply > 0.1f)
-            {
-                speedMultiply -= 0.1f;
-                //Debug.Log("Increase SM: " +speedMultiply);
-            }
-        }
-
-        if (playerID == 3 && (moveX || moveZ))
-        {
-            Vector3 tmp = InputManager.FourMainJoystick();
-            if (tmp != new Vector3(0, 0, 0))
-            {
-                speedMulti();
-                wallTest(playerList[3]);
-                //Debug.Log("move" + body.name);
-                body.transform.Translate(tmp.x * speedMultiply * Time.deltaTime, 0, tmp.z * speedMultiply * Time.deltaTime);
-            }
-            else if (speedMultiply > 0.1f)
-            {
-                speedMultiply -= 0.1f;
-                //Debug.Log("Increase SM: " +speedMultiply);
-            }
-        }
-
-
+       
         // Speed Verfall
         if (speedMultiply < 0.01f)
         {
@@ -189,7 +264,7 @@ public class PlayerScript : MonoBehaviour
 
     }
 
-
+   
     public void setWorld(WorldScript world)
     {
         this.world = world;
@@ -334,13 +409,13 @@ public class PlayerScript : MonoBehaviour
 
     void wallTest(GameObject player)
     {
-        
+
         int xPos = (int)Mathf.Round(player.transform.position.x);
         int zPos = (int)Mathf.Round(player.transform.position.z);
 
         if (world.WorldArray[xPos, zPos] != null)
         {
-            Debug.Log("Object an aktueller Stelle: " +world.WorldArray[xPos, zPos]);
+            //Debug.Log("Object an aktueller Stelle: " + world.WorldArray[xPos, zPos]);
             if (world.WorldArray[xPos, zPos].name == "Item_SpeedBoost")
             {
                 Destroy(world.WorldArray[xPos, zPos]);
@@ -355,9 +430,12 @@ public class PlayerScript : MonoBehaviour
         else
         {
             world.WorldArray[xPos, zPos] = player;
-            Debug.Log("Object an aktueller Stelle: Freier Weg");
+            //Debug.Log("Object an aktueller Stelle: Freier Weg");
         }
     }
 
+    
 }
+
+
 
