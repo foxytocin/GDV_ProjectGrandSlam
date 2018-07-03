2730885a181f941e53e0601158eba13fdab463a7
foxytocin <info@anfuchs.de>
2018-07-04 00:37:53 +0200

    Merge korrigiert

--- Prototyp_GAME_Version 0.04/Assets/Script/BombSpawner.cs
+++ Prototyp_GAME_Version 0.04/Assets/Script/BombSpawner.cs
@@ -1,40 +1,7 @@
 ﻿using UnityEngine;
 
 public class BombSpawner : MonoBehaviour
-<<<<<<< HEAD
-{
-    public LevelGenerator LevelGenerator;
-    public GameObject Bomb_Prefab;
-    public PlayerSpawner PlayerSpawner;
 
-    public void SpawnBomb(int id)
-    {
-        PlayerScript player = PlayerSpawner.playerList[id].gameObject.GetComponent<PlayerScript>();
-        int xPos = (int)Mathf.Round(player.transform.position.x);
-        int zPos = (int)Mathf.Round(player.transform.position.z);
-
-        player.setAvaibleBomb(-1);
-
-        GameObject bombeInstanz = Instantiate(Bomb_Prefab, new Vector3(xPos, 0.4f, zPos), Quaternion.identity);
-        BombScript thisBombeScript = bombeInstanz.GetComponent<BombScript>();
-
-        thisBombeScript.playerList = player.getPlayerList();
-        thisBombeScript.bombTimer = player.getbombTimer();
-        thisBombeScript.bombOwnerPlayerID = id;
-        thisBombeScript.bombPower = player.getRange();
-        thisBombeScript.remoteBomb = player.getRemoteBomb();
-        thisBombeScript.tag = "Bombe";
-
-        player.creatingBomb = false;
-
-        LevelGenerator.AllGameObjects[xPos,zPos] = bombeInstanz;
-
-        if(thisBombeScript.remoteBomb) {
-            PlayerSpawner.playerList[id].gameObject.GetComponent<PlayerScript>().remoteBombList.Add(bombeInstanz);
-            Debug.Log("Remote Bombe zur Liste hinzugefügt" +player.remoteBombList.Count);
-        }
-    }
-=======
 {
     public LevelGenerator LevelGenerator;
     public GameObject Bomb_Prefab;
@@ -66,5 +33,4 @@ public class BombSpawner : MonoBehaviour
 
         player.creatingBomb = false;
     }
->>>>>>> master
 }
