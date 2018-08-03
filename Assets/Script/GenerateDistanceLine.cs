aeac5c09db29f9c88aa06d0b5f786ce49e638b2d
Andreas Fuchs <info@anfuchs.de>
2018-08-03 18:14:11 +0200

    Neue Funktion: DistanceLine alle 20 Meter (zum Test so oft)
    Um den Spielern den Fortschitt im Level zu zeigen, wird alle X Meter eine DistanceLinie generiert. Diese wir das Level procedural erstellt und passt sich automatisch an den jeweiligen Levelabschnitt an. Start- und Endpunkt der Linie wird berechnet. Die Querstange zwischen de Pfosten entsprechend skaliert und platziert.

---
+++ Assets/Script/GenerateDistanceLine.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateDistanceLine : MonoBehaviour {
+
+public LevelGenerator LevelGenerator;
+public GameObject StangePrefab;
+public GameObject QuerstrebePrefab;
+private int startPoint;
+private int endPoint;
+private int centerPoint;
+private int scaleQuerstrebe;
+
+	void Start()
+	{
+		startPoint = 0;
+		endPoint = 0;
+	}
+
+	public void createDistanceLine(int row)
+	{
+		bool foundStart = false;
+		for (int i = 0; i < LevelGenerator.levelSectionData[0].Length - 1; i++)
+		{
+			if(LevelGenerator.AllGameObjects[i, row] != null)
+			{
+				//Begin des Levels suchen (das erste Mauerstück)
+				if(LevelGenerator.AllGameObjects[i, row].gameObject.CompareTag("Wand") && !foundStart) {
+					foundStart = true;
+					startPoint = i;
+				}
+			}
+
+			//Ende des Levels suchen (das letzte Mauerstück)
+			if(LevelGenerator.AllGameObjects[i, row] != null && foundStart) {
+				endPoint = i;
+			}
+
+			centerPoint = startPoint + (endPoint - startPoint) / 2;
+			scaleQuerstrebe = endPoint - startPoint - 1;
+		}
+
+		GameObject StartStange = Instantiate(StangePrefab, new Vector3(startPoint, 1f, row), Quaternion.Euler(0f, -90f, 0f), transform);
+		GameObject EndStange = Instantiate(StangePrefab, new Vector3(endPoint, 1f, row), Quaternion.Euler(0f, 90f, 0f), transform);
+		GameObject Querstrebe = Instantiate(QuerstrebePrefab, new Vector3(centerPoint, 4f, row), Quaternion.Euler(0f, 90f, 0f), transform);
+		Querstrebe.transform.localScale = new Vector3(1f, 1f, scaleQuerstrebe);
+
+		LevelGenerator.DistanceLines[0, row] = StartStange;
+		LevelGenerator.DistanceLines[1, row] = EndStange;
+		LevelGenerator.DistanceLines[2, row] = Querstrebe;
+	}
+}
