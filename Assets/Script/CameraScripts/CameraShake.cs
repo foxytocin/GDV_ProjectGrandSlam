b54287bc10010612852384c998d4ce6244135278
Andreas Fuchs <info@anfuchs.de>
2018-08-14 01:23:49 +0200

    PostProcessingEditing: Optimierung / CameraShake: ChromaticAberration
    PostProcessingEditing: Optimierung
    - FogMode von Linear auf ExponentialSquared umgestellt
    - ChromaticAberration implementiert
    - Zugriff auf PostProcessingProfile
    
    CameraShake: ChromaticAberration
    - ChromaticAberration implementiert
    - Zugriff auf ChromaticAberrationStength im PostProcessingEditing
    
    Beim explodieren der Bombe wird nebem dem Shake auch die chromatische Aberration angesteurt und entsprechend der Shake-Intensitiy dargestellt. Das heißt sie nimmt ebenso exponentiell ab wie das Wackeln.

--- Assets/Script/CameraScripts/CameraShake.cs
+++ Assets/Script/CameraScripts/CameraShake.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour {
 
+	private PostProcessingEditing postProcessingEditing;
+
+	void Awake()
+	{
+		postProcessingEditing = FindObjectOfType<PostProcessingEditing>();
+	}
+
 	public IEnumerator Shake(float duration, float intensity)
 	{
 		float timeMoved = 0f;
-		float intensityBasis = intensity;
 
 		while(timeMoved < duration)
 		{
@@ -17,12 +23,9 @@ public class CameraShake : MonoBehaviour {
 			transform.localPosition = new Vector3(x, y, 0f);
 			timeMoved += Time.deltaTime;
 
-			//Intensity nimmt prozentual zur vergangenen Zeit ab. So wird die Bewegung "ausgebremst"Mathf.Pow(speed,i);
-			//intensity = intensityBasis * (duration - (duration * timeMoved));
-
-			//intensity = 0.4f * (duration - (duration * timeMoved));
-
+			//Intensity nimmt exponentiell ab
 			intensity = intensity * Mathf.Pow(0.4f, timeMoved);
+			postProcessingEditing.chromaticAberrationStrength = intensity * 3f;
 
 			//Debug.Log("ShakeTimeMoved: " +timeMoved);
 			//Debug.Log("ShakeIntensivity: " +intensity);
@@ -31,5 +34,6 @@ public class CameraShake : MonoBehaviour {
 		}
 
 		transform.localPosition = new Vector3(0f, 0f, 0f);
+		postProcessingEditing.chromaticAberrationStrength = 0f;
 	}
 }
--- Assets/Script/PostProcessingEditing.cs
+++ Assets/Script/PostProcessingEditing.cs
@@ -7,8 +7,10 @@ public class PostProcessingEditing : MonoBehaviour {
 
     private PostProcessingProfile pp;
     private DepthOfFieldModel.Settings depthSettings;
-
+    private ChromaticAberrationModel.Settings chromAbSettings;
+    public float chromaticAberrationStrength;
     private CameraDirection cameraDirection;
+
 
     // Use this for initialization
     void Start () {
@@ -17,21 +19,30 @@ public class PostProcessingEditing : MonoBehaviour {
         //Change PostProcessing Settings
         pp = GetComponent<PostProcessingBehaviour>().profile;
         pp.bloom.enabled = true;
-
+  
         depthSettings = pp.depthOfField.settings;
         depthSettings.focalLength = 110;
         depthSettings.aperture = 7f;
 
         //Initialize Fog
         RenderSettings.fog = true;
-        RenderSettings.fogMode = FogMode.Linear;
-        RenderSettings.fogStartDistance = 23f;
-        RenderSettings.fogEndDistance = 31f;
+        RenderSettings.fogMode = FogMode.ExponentialSquared;
+        RenderSettings.fogDensity = 0.035f;
+        //RenderSettings.fogStartDistance = 13f;
+        //RenderSettings.fogEndDistance = 51f;
+
+        //Initialize ChromaticAberration
+        chromaticAberrationStrength = 0f;
+        pp.chromaticAberration.enabled = true;
+        chromAbSettings = pp.chromaticAberration.settings;
+        chromAbSettings.intensity = chromaticAberrationStrength;
+        chromAbSettings.spectralTexture = null;
     }
 
     // Update is called once per frame
     void LateUpdate () {
         setFocusPoint();
+        setChromaticAberrationStrength();
     }
 
     void setFocusPoint()
@@ -39,5 +50,11 @@ public class PostProcessingEditing : MonoBehaviour {
         float distance = Vector3.Distance(this.transform.position, cameraDirection.target);
         depthSettings.focusDistance = distance;
         pp.depthOfField.settings = depthSettings;
+    }
+
+    void setChromaticAberrationStrength()
+    {
+        chromAbSettings.intensity = chromaticAberrationStrength;
+        pp.chromaticAberration.settings = chromAbSettings;
     }
 }
