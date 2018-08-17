2e4c18dcc1a3d9a77017cbb6aafa69aeaba6a8f8
Andreas Fuchs <info@anfuchs.de>
2018-08-17 16:01:00 +0200

    Tag / Nach Wechsel implementiert (noch nicht ganz fertig)
    Im GameManager kann der Haken auf NACHT gesetzt werden.
    Dann wird es langsam dunkle und es werden prozedural Lampen auf den Mauerstücken generiert. Timing ist noch nicht fertig. Lampen sind aktuell erst nach 60 LevelLinien sichtbar.

---
+++ Assets/Script/LevelScripts/NightModus.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightModus : MonoBehaviour {
+
+	public bool nightModus;
+	private bool isDay;
+	private LevelGenerator levelGenerator;
+	private Light worldLight;
+	private float worldLightIntensivity;
+
+	void Awake()
+	{
+		levelGenerator = FindObjectOfType<LevelGenerator>();
+		worldLight = FindObjectOfType<Light>();
+	}
+
+	void Start()
+	{
+		isDay = true;
+		worldLightIntensivity = worldLight.intensity;
+	}
+
+	void Update()
+	{
+		if(nightModus && isDay)
+		{
+			isDay = false;
+			switchToNight();
+			Debug.Log("Switch to Night");
+		}
+
+		if(!nightModus && !isDay)
+		{
+			isDay = true;
+			switchToDay();
+			Debug.Log("Switch to Day");
+
+		} 
+	}
+
+	private void switchToNight()
+	{
+		StopAllCoroutines();
+		levelGenerator.generateGlowBalls = true;
+		RenderSettings.ambientIntensity = 0.5f;
+		StartCoroutine(dawnWorldLight(0f));
+	}
+
+	private void switchToDay()
+	{
+		StopAllCoroutines();
+		levelGenerator.generateGlowBalls = false;
+		RenderSettings.ambientIntensity = 2f;
+		StartCoroutine(riseWorldLight(worldLightIntensivity));
+	}
+
+	private IEnumerator dawnWorldLight(float value)
+	{
+		while(worldLight.intensity > value)
+		{
+			worldLight.intensity -= Time.deltaTime;
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	private IEnumerator riseWorldLight(float value)
+	{
+		while(worldLight.intensity < value)
+		{
+			worldLight.intensity += Time.deltaTime;
+			yield return new WaitForEndOfFrame();
+		}
+		worldLight.intensity = value;
+	}
+}
