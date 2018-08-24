0a932b1f859398c5e9cc4ecca3916146f4f52906
philippkoerber98 <philippkoerber1998@gmail.com>
2018-08-24 22:28:52 +0200

    Sound beim Spawnen eines PowerUps

--- Assets/Script/ItemScripts/ItemSpawner.cs
+++ Assets/Script/ItemScripts/ItemSpawner.cs
@@ -7,14 +7,22 @@ public class ItemSpawner : MonoBehaviour
     public LevelGenerator LevelGenerator;
     public GameObject Item_Prefab;
     public GameObject ItemObjekt;
+    public AudioSource audioSource;
+    public AudioClip ItemAppears;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
 
-    public void SpawnItem(int x, int z)
+        public void SpawnItem(int x, int z)
     {
         //Position des Items = Position der zerstörten Kiste.
         ItemObjekt = Instantiate(Item_Prefab, new Vector3(x, 0.7f, z), Quaternion.identity, transform);
         LevelGenerator.AllGameObjects[x, z] = ItemObjekt;
- 
+        audioSource.PlayOneShot(ItemAppears, 0.5f);
+
     }
 
 
