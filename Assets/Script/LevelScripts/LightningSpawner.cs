c3d4d640de60c29dd8cfeabe46229af3366d984f
Joshua Hirsch <40173207+ItsJoshuaHirsch@users.noreply.github.com>
2018-08-24 17:03:28 +0200

    Fixed Lightning_Prefab

--- Assets/Script/LevelScripts/LightningSpawner.cs
+++ Assets/Script/LevelScripts/LightningSpawner.cs
@@ -18,7 +18,7 @@ public class LightningSpawner : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         
-        if(cameraScroller.rowPosition > tmp)
+        if(cameraScroller.rowPosition > tmp*2)
         {
             tmp = cameraScroller.rowPosition;
             GameObject lightning = Instantiate(lightning_prefab, new Vector3(0, 0, cameraScroller.rowPosition), Quaternion.identity);
