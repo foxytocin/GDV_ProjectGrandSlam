4ce5b470a310f0a5340cb2f869671b8e0cd4c1e2
Andreas Fuchs <info@anfuchs.de>
2018-08-30 22:30:50 +0200

    Sync Fix

--- Assets/Script/MenuScripts/NextRoundButtonScript.cs
+++ Assets/Script/MenuScripts/NextRoundButtonScript.cs
@@ -5,30 +5,18 @@ using UnityEngine;
 public class NextRoundButtonScript : MonoBehaviour {
 
     public GameObject resultMenu;
-<<<<<<< HEAD
-=======
-
-    RulesScript rulesScript;
     GameManager gameManager;
->>>>>>> 7560a9515629792e4f73735e71a2d461f130b221
     LevelRestart levelRestart;
     
     void Start()
     {
         levelRestart = FindObjectOfType<LevelRestart>();
-        rulesScript = FindObjectOfType<RulesScript>();
     }
 
     public void onClickNextButton()
     {
         levelRestart.levelRestartNextRound();
         FindObjectOfType<OverlayMethodenScript>().isInGame = true;
-<<<<<<< HEAD
-=======
-
-        rulesScript.nextRoundRules();
-        gameManager.unlockControlls();
->>>>>>> 7560a9515629792e4f73735e71a2d461f130b221
         resultMenu.SetActive(false);
     }
 
