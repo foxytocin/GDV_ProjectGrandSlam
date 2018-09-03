80f33cd5f75de70222bb454bff213cac708faa3b
fdai4076 <sebastian.krah2@informatik.hs-fulda.de>
2018-09-03 18:44:55 +0200

    Alter Counter entfernt und neuen länger gemacht

--- Assets/Script/LevelScripts/CounterScript.cs
+++ Assets/Script/LevelScripts/CounterScript.cs
@@ -5,13 +5,6 @@ using TMPro;
 
 public class CounterScript : MonoBehaviour
 {
-
-    public GameObject counterObject;
-    public TextMeshPro counterTxt;
-    public Light mainLight;
-    public Light spotlight;
-    public Light Blue;
-
     private GameManager gameManager;
     private Vector3 tmpKoord;
     private float tmpLightIntensity;
@@ -20,8 +13,6 @@ public class CounterScript : MonoBehaviour
     {
         levelGenerator = FindObjectOfType<LevelGenerator>();
         gameManager = FindObjectOfType<GameManager>();
-        tmpKoord = counterObject.transform.position;
-        tmpLightIntensity = mainLight.intensity;
     }
 
 
@@ -67,69 +58,10 @@ public class CounterScript : MonoBehaviour
                         break;
                 }
             }
-            yield return new WaitForSecondsRealtime(1f);
+            yield return new WaitForSecondsRealtime(1.5f);
             counter--;
         }
         
     }
 
-    /*
-    private IEnumerator startCounterCore()
-    {
-        while(mainLight.intensity > 0.2f)
-        {
-            Debug.Log("light");
-            mainLight.intensity -= (Time.deltaTime + 0.2f);
-            yield return null;
-        }
-
-        spotlight.intensity = 5;
-        int counter = 3;
-
-        counterObject.SetActive(true);
-
-        float endKoord = 8f;
-
-        while (counter >= 0)
-        {
-            if (counter == 0)
-            {
-                counterTxt.SetText("Let´s Go!");
-            }
-            else
-            {
-                counterTxt.SetText(counter.ToString());
-            }
-            counterObject.transform.position = tmpKoord;
-            StartCoroutine(animatorCounter(tmpKoord.z, endKoord));
-            counter--;
-            yield return new WaitForSecondsRealtime(1);
-        }
-
-        spotlight.intensity = 0f;
-
-        while (mainLight.intensity < tmpLightIntensity)
-        {
-            Debug.Log("light");
-            mainLight.intensity += (Time.deltaTime + 0.2f);
-            yield return null;
-        }
-
-        counterObject.SetActive(false);
-        FindObjectOfType<AudioManager>().playSound("lets_go");
-        gameManager.unlockControlls();
-
-    }
-
-    IEnumerator animatorCounter(float begin, float end)
-    {
-
-        while(begin < end)
-        {
-            counterObject.transform.Translate(0, 0, 0.2f * (Time.deltaTime + 0.2f));
-            yield return null;
-        }
-
-    }
-    */
 }
