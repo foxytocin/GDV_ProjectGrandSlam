9b27c2aa6a1fc860338e36b0b81d63ff0e9c71a1
Philipp PC <philipp.koerber1998@gmail.com>
2018-09-08 22:33:00 +0200

    Überarbeiten des BombRainItems: Hinzufügen der CheckLevel Funktion

--- Assets/Script/ItemScripts/BombRain.cs
+++ Assets/Script/ItemScripts/BombRain.cs
@@ -5,32 +5,21 @@ using UnityEngine;
 public class BombRain : MonoBehaviour
 {
 
-    ObjectPooler objectPooler;
-    public GameObject KistenPartsPrefab;
     private CameraScroller cameraScroller;
     private LevelGenerator levelGenerator;
-    private MapDestroyer mapDestroyer;
-    private AudioManager audioManager;
-    public AudioSource audioSource;
-    public AudioClip bombRainSound1;
-    public AudioClip bombRainSound2;
-    public AudioClip bombRainSound3;
     public bool bombenregen = false;
     private BombSpawner bombSpawner;
     public Color32 bombraincolor;
     public int PlayerID;
+    public float bombYPos = 10f;
 
 
     void Awake()
     {
-        bombraincolor = new Color32(0, 0, 0, 0);
         bombSpawner = FindObjectOfType<BombSpawner>();
-        objectPooler = ObjectPooler.Instance;
+        bombraincolor = new Color32(0, 0, 0, 0);
         cameraScroller = FindObjectOfType<CameraScroller>();
-        mapDestroyer = FindObjectOfType<MapDestroyer>();
         levelGenerator = FindObjectOfType<LevelGenerator>();
-        audioSource = FindObjectOfType<AudioSource>();
-        audioManager = FindObjectOfType<AudioManager>();
     }
 
     void FixedUpdate()
@@ -51,12 +40,13 @@ public class BombRain : MonoBehaviour
         int z = bombPos.z;
 
         GameObject go;
+        GameObject bombego;
 
         if (levelGenerator.SecondaryGameObjects1[x, z] != null)
         {
             if (levelGenerator.SecondaryGameObjects1[x, z].gameObject.CompareTag("Boden") && levelGenerator.AllGameObjects[x, z] == null)
             {
-                levelGenerator.AllGameObjects[x, z] = bombSpawner.SpawnBomb(bombPos.x, bombPos.z, 5, 1, 3, false, true, bombraincolor);
+                
             }
 
             if (levelGenerator.AllGameObjects[x, z] != null)
@@ -66,26 +56,17 @@ public class BombRain : MonoBehaviour
                 switch (go.tag)
                 {
                     case "Kiste":
-                        go.SetActive(false);
-
-                        //Ersetzt die Kiste durch Kiste_destroyed Prefab
-                        Instantiate(KistenPartsPrefab, new Vector3(x, 0.5f, z), Quaternion.identity, transform);
-                        levelGenerator.AllGameObjects[bombPos.x, bombPos.z] = bombSpawner.SpawnBomb(bombPos.x, bombPos.z, 5, 1, 3, false, true, bombraincolor);
+                        bombego = bombSpawner.SpawnBomb(bombPos.x, bombYPos, bombPos.z, 5, 1, 3, false, true, bombraincolor);
 
                         break;
 
                     case "Item":
-                        audioManager.playSound("break2");
-                        go.SetActive(false);
-                        levelGenerator.AllGameObjects[bombPos.x, bombPos.z] = bombSpawner.SpawnBomb(bombPos.x, bombPos.z, 5, 1, 3, false, true, bombraincolor);
+                        bombego = bombSpawner.SpawnBomb(bombPos.x, bombYPos, bombPos.z, 5, 1, 3, false, true, bombraincolor);
 
                         break;
 
                     case "Player":
-                        objectPooler.SpawnFromPool("Explosion", new Vector3(x, 0.5f, z), Quaternion.identity);
-                        go.GetComponent<PlayerScript>().dead();
-                        StartCoroutine(mapDestroyer.KillField(x, z));
-                        levelGenerator.AllGameObjects[bombPos.x, bombPos.z] = bombSpawner.SpawnBomb(bombPos.x, bombPos.z, 5, 1, 3, false, true, bombraincolor);
+                        bombego = bombSpawner.SpawnBomb(bombPos.x, bombYPos, bombPos.z, 5, 1, 3, false, true, bombraincolor);
                         break;
 
                     case "Wand":
@@ -105,13 +86,5 @@ public class BombRain : MonoBehaviour
     }
 
 
-    void BombRainSound()
-    {
-        switch ((int)Random.Range(0f, 3f))
-        {
-            case 0: audioSource.PlayOneShot(bombRainSound1, 0.5f * audioManager.settingsFXVolume); break;
-            case 1: audioSource.PlayOneShot(bombRainSound2, 0.5f * audioManager.settingsFXVolume); break;
-            case 2: audioSource.PlayOneShot(bombRainSound3, 0.5f * audioManager.settingsFXVolume); break;
-        }
-    }
+
 }
