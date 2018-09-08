c2ab02a82a915be18fb6c070ad7b3e2325fc978b
Philipp PC <philipp.koerber1998@gmail.com>
2018-09-08 22:31:52 +0200

    BombSpawner überarbeitet um in zukunft fallende bomben abzuwerfen (funktioniert noch nicht)

--- Assets/Script/BombScripts/BombSpawner.cs
+++ Assets/Script/BombScripts/BombSpawner.cs
@@ -2,11 +2,12 @@
 
 public class BombSpawner : MonoBehaviour
 {
-    public GameObject SpawnBomb(int bombXPos, int bombZPos, int playerId, int bombPower, float bombTimer, bool remoteBomb, bool bombrain, Color playerColor)
+    public GameObject SpawnBomb(int bombXPos, float bombYPos, int bombZPos, int playerId, int bombPower, float bombTimer, bool remoteBomb, bool bombrain, Color playerColor)
     {
         // Erlaubt das Legen einer Bombe, wenn das Feld frei (null) ist, oder der Spieler ("Player") selbst sich auf diesem befindet.
-        GameObject bombeInstanz = ObjectPooler.Instance.SpawnFromPool("Bombe", new Vector3(bombXPos, 0.38f, bombZPos), Quaternion.identity);
+        GameObject bombeInstanz = ObjectPooler.Instance.SpawnFromPool("Bombe", new Vector3(bombXPos, bombYPos, bombZPos), Quaternion.identity);
         BombScript thisBombeScript = bombeInstanz.GetComponent<BombScript>();
+        RainingBomb thisRainingBomb = bombeInstanz.GetComponent<RainingBomb>();
 
         // PlayerStats werden auf die gelegte Bombe uebertragen, damit sie ihr individuelles Verhalten bekommt.
         thisBombeScript.bombTimer = bombTimer;
@@ -17,6 +18,10 @@ public class BombSpawner : MonoBehaviour
         thisBombeScript.playerColor = playerColor;
 
         StartCoroutine(thisBombeScript.bombAnimation());
+        if (bombrain == true)
+        {
+            StartCoroutine(thisRainingBomb.BombFall(bombXPos, bombZPos));
+        }
 
         return bombeInstanz;
     }
